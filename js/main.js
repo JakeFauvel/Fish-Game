114192337b54f96246ffeb81ec4f0fb09b4dfb70
Jake Fauvel <jakefauvel@msn.com>
2018-11-07 22:09:56 +0000

    Basic Phaser functions in place. background being loaded in.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-let Test = require("./test.js");
+let Phaser = require("Phaser");
 
-Test.testing();
+
+let config = {
+    type: Phaser.AUTO,
+    width: 1250,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('bg1_small', './assets/bg1_small.png');
+}
+
+function create ()
+{
+    this.add.image(625, 300, 'bg1_small');
+}
+
+function update ()
+{
+
+}
