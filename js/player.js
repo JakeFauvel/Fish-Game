3bd03b43b36ff5785d709a484c3f086378ea626c
Jake Fauvel <jakefauvel@msn.com>
2018-12-13 22:27:23 +0000

    Player movement in place - following mouse on game start. Player sprite is being flipped on direction change (left or right).

--- js/player.js
+++ js/player.js
@@ -6,24 +6,53 @@ let game = undefined;
 let layer = undefined;
 let player = undefined;
 
+let directions = ["W", "NW", "N", "NE", "E", "SE", "S", "SW"];
+
 let preload = function(inputGame) {
     game = inputGame;
     game.load.spritesheet('player', './assets/red_angler_idle.png', PLAYER_WIDTH, PLAYER_HEIGHT, 19);
 };
 
-let create = function(inputLayer) {
+let create = function() {
     layer = game.add.group();
     player = layer.create(centerPlayerX(), centerPlayerY(), 'player');
-    console.log(centerPlayerX(), centerPlayerY());
+    game.physics.enable(player, Phaser.Physics.ARCADE);
     player.alpha = 0;
     player.animations.add('idle');
     player.scale.setTo(playerScale, playerScale);
+    player.anchor.setTo(0.5);
     player.animations.play('idle', 15, true);
 };
 
 let update = function() {
-
+    playerMovement();
 };
+
+function playerMovement() {
+    let movement = game.physics.arcade.moveToPointer(player, 400);
+    if (getDirection(movement) === 'W') {
+        player.scale.setTo(playerScale, playerScale);
+    } else {
+        player.scale.setTo(-playerScale, playerScale);
+    }
+    checkForMouseContact();
+}
+
+function getDirection(angle, allowDiagonal) {
+    if (allowDiagonal) {
+        angle = Math.round((angle + Math.PI) / (Math.PI * 2) * 8) % 8;
+        return (directions[angle]);
+    } else {
+        angle = Math.round((angle + Math.PI) / (Math.PI * 2) * 4) % 4;
+        return (directions[angle * 2]);
+    }
+}
+
+function checkForMouseContact() {
+    if (Phaser.Rectangle.contains(player.body, game.input.x, game.input.y)) {
+        player.body.velocity.setTo(0, 0);
+    }
+}
 
 function centerPlayerX() {
     return game.world.centerX - (PLAYER_WIDTH * playerScale) / 2;
